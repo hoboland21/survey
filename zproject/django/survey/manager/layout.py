945f5d6c63181bfc374b07e6a4177e29169f2e41
JC Clark <jc@saipantech.com>
2019-04-26 22:24:15 +1000

    up to selecting questions

--- zproject/django/survey/manager/layout.py
+++ zproject/django/survey/manager/layout.py
@@ -26,6 +26,21 @@ def layout(request) :
         if layout.is_valid():
             ll.survey = survey
             ll.save()
+    if "edit_layout" in request.POST :
+        layout = Layout.objects.get(id=request.POST["edit_layout"])
+        result['layout_form'] = LayoutForm(instance=layout)
+        result["layout"] = layout
+        result["questions"] = Question.objects.all().order_by("label")
+        result["items"] = Items.objects.filter(layout__id=layout.id)
+    if "add_selected" in request.POST  and "q_select" in request.POST :
+        seq = 10
+        for q in request.POST.getlist("q_select") :
+            i = Items()
+            i.layout = layout
+            i.question = Question.objects.get(id=q)
+            i.sequence = seq 
+            seq +=  5
+            i.save()
     if survey :
         result["layouts"] = Layout.objects.filter(survey__id=survey.id)
     return render(request,'manager/layout.html',context=result)
