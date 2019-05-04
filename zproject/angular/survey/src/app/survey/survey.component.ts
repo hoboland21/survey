830aa6a7719a93b97be20d8bb60ef378d7a69fe6
JC Clark <jc@saipantech.com>
2019-05-04 12:17:05 +1000

    tt

--- zproject/angular/survey/src/app/survey/survey.component.ts
+++ zproject/angular/survey/src/app/survey/survey.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { SurveyService } from '../survey/survey.service';
 import { ISurvey } from '../survey/survey';
@@ -20,27 +20,34 @@ export class SurveyComponent implements OnInit {
   student: IStudent;
   answers: IAnswer[];
   page_number = 0;
-
+  isSubmitted = false;
   errorMessage = '';
  
   constructor(
     private _router:  Router,
     private _route:   ActivatedRoute,
-    private formBuilder: FormBuilder,
     private surveyService: SurveyService
   ) { }
-  answerForm : FormGroup;
   pageForward() {
     
     if (this.page_number < this.questions.length) {
       this.page_number += 1;
+      console.log("number bump")
     }
   }
-
-  
+  submitForm(form:NgForm) {
+    this.isSubmitted = true;
+    if(!form.valid) {
+      return false;
+    } else {
+      this.pageForward();
+     
+    }
+  }
   ngOnInit() {
     this.id = Number(this._route.snapshot.paramMap.get('id'));
-    
+
+  
     this.surveyService.getSurvey(this.id).subscribe(
       survey => this.survey = survey,
       error => this.errorMessage = <any>error
