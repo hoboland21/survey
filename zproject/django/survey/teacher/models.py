a3c0c5ccd9913bd373f953d22383e626f45d094c
JC Clark <jc@saipantech.com>
2019-05-15 21:48:24 +1000

    db adjusted

--- zproject/django/survey/teacher/models.py
+++ zproject/django/survey/teacher/models.py
@@ -3,7 +3,13 @@ import datetime
 
 # Create your models here.
 
-
+class Course(models.Model) :
+    department  =   models.CharField(max_length=128,blank=True)
+    title       =   models.CharField(max_length=256,blank=True)
+    name        =   models.CharField(max_length=256,blank=True)
+    section     =   models.CharField(max_length=256,blank=True)
+    instructor  =   models.CharField(max_length=256,blank=True)
+  
 class Survey(models.Model):
     name            =   models.CharField(max_length=256)
     requester       =   models.CharField(max_length=256)
@@ -22,22 +28,16 @@ class Question(models.Model):
     sequence        =   models.SmallIntegerField()
     created         =   models.DateTimeField(auto_now_add=True)
 
-class Course(models.Model) :
-    department  =   models.CharField(max_length=128,blank=True)
-    title       =   models.CharField(max_length=256,blank=True)
-    name        =   models.CharField(max_length=256,blank=True)
-    section     =   models.CharField(max_length=256,blank=True)
-    instructor  =   models.CharField(max_length=256,blank=True)
-  
 class Student(models.Model):
     survey          =   models.ForeignKey(Survey, on_delete=models.CASCADE)
-    test_code       =   models.CharField(max_length=32)
-    course          =   models.ForeignKey(Course, on_delete=models.CASCADE)
     name            =   models.CharField(max_length=128)
+    grade_level     =   models.CharField(max_length=4)
+    test_code       =   models.CharField(max_length=32)
     created         =   models.DateTimeField(auto_now=True)
 
 class Answer(models.Model):
     student         =   models.ForeignKey(Student, on_delete=models.CASCADE)
+    course          =   models.ForeignKey(Course, on_delete=models.CASCADE)
     question        =   models.ForeignKey(Question, on_delete=models.CASCADE)
     answer          =   models.CharField(max_length=10)
 
